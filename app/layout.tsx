e38cac8d6e4c2a59a05239732139cb4c06aa9a4b
bomvoi14 <63687038+bomvoi14@users.noreply.github.com>
2026-09-11 20:02:41 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,13 @@ import type { Metadata } from "next";
 import { Kanit } from "next/font/google";
 import "./globals.css";
 
-// นำเข้าฟอนต์ Kanit จาก Google Fonts
 const kanit = Kanit({ 
   subsets: ["latin", "thai"],
   weight: ['300', '400', '500', '700'] 
 });
 
 export const metadata: Metadata = {
-  title: "Site Schedule App",
+  title: "จำนวนวันตามคำสั่ง",
   description: "แอปพลิเคชันดูตารางออกไซต์งาน",
 };
 
@@ -20,7 +19,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="th">
-      {/* ใส่ className ของฟอนต์ที่ body เพื่อให้ใช้ทั้งแอป */}
       <body className={`${kanit.className} bg-gray-100 text-gray-900`}>
         {children}
       </body>
